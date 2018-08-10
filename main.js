6d09ad3b8088a480b8b15ec304126eabb72ec3d2
Mishal Alsharekh <mishal.sharekh@gmail.com>
2018-08-11 01:57:49 +0300

    second commit with success2

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block{
-
     constructor(index, timestamp, data, previousHash=''){
         this.index=index;
         this.timestamp=timestamp;
